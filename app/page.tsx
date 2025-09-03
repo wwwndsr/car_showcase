05e2923e337091bc076f4bcaebe3c3ca103ca593
wwwndsr <cristina.perepeliuc@isa.utm.md>
2025-09-03 20:30:37 +0300

    новые компоненты, хэдэр футер

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
+
+import { Hero } from "@/components";
 
 export default function Home() {
   return ( 
       <main className="overflow-hidden">
-
+        <Hero />
       </main>
-  );
+  )
 }
